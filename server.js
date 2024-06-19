712461d9b0bb0d690421c05d0545273615d1e3fc
Fadi ANTWAN <antwan.fadi95@gmail.com>
2024-06-19 09:08:13 +0200

    node-dotenv-04 test

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 require("dotenv").config();
 
 // this line is mandatory when you want to read variables from the '.env' file
-console.log(
-  `I am ${process.env.MY_NAME}, wilder in ${process.env.MY_CITY}, and I Love ${process.env.MY_LANGUAGE}`
-);
+
+const name = process.env.MY_NAME;
+const city = process.env.MY_CITY;
+const language = process.env.MY_LANGUAGE;
+
+console.log(`I am ${name}, wilder in ${city}, and I Love ${language}`);
